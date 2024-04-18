ef6f0733624aed7bb09ba767b3406a920baedad4
Biser Svilenov Stanev <biser@MacBook-Air-de-Biser.local>
2024-04-18 20:52:46 +0200

    add new configuration database

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const dbConnection = require('./config/config')
 const { get } = require('mongoose')
 const app = express()
 require('dotenv').config()
 PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => (res.send('Proyecto BACK')))
+
+dbConnection()
 
 app.listen(PORT, () => {
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
